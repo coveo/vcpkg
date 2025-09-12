b20d2e59f6cc53fde07e4aa470c4866469b107ee
Benoit Lavallee <blavallee@coveo.com>
2025-09-12 14:19:10 -0400

    [aws-sdk-cpp] Bump to 1.11.646

--- ports/aws-sdk-cpp/portfile.cmake
+++ ports/aws-sdk-cpp/portfile.cmake
@@ -4,13 +4,13 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO aws/aws-sdk-cpp
     REF "${VERSION}"
-    SHA512 0cdc0f6e0499dd71603a19b86ac6c4e0f918687496349e9a1a44ee3e66f86252663494ccadc9a49f2e12b8162ea469a9639e63c594fb98c7b5a409fd2f74eaa8
+    SHA512 27bbac3834cbf36842395429520c19491cc9e67ae582c53c2c49789eb36daa3ee74754d010e554e9117a2fd4242e0df9e6db600976786ed8aebc45200b1849cf 
     PATCHES
         fix-aws-root.patch
         lock-curl-http-and-tls-settings.patch
         fix_find_curl.patch
         find-dependency.patch
-        configure-binary-dir.patch # https://github.com/aws/aws-sdk-cpp/pull/3459
+	configure-binary-dir.patch # https://github.com/aws/aws-sdk-cpp/pull/3459
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" FORCE_SHARED_CRT)
